f8c88cd5630613241343a48efe2412b83ec7da81
Aleeg0 <Sankafoxe@gmail.com>
2024-07-08 21:53:16 +0300

    add header stylesheet

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import Header from "./Components/Header";
+import "./index.css"
 
 function App() {
-
+    return (
+      <div className="wrapper">
+          <Header/>
+          <div>
+              <h1>ALL Sneakers</h1>
+          </div>
+      </div>
+    );
 }
 
 export default App;
