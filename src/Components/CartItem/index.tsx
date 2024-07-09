5d99b6a1fe5f30244869d7a7aab11d3daac081e5
Aleeg0 <Sankafoxe@gmail.com>
2024-07-09 19:20:19 +0300

    add get logic from backend

--- src/Components/CartItem/index.tsx
+++ src/Components/CartItem/index.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import img1 from "../../assets/images/image1.jpg"
 import {ReactComponent as RemoveBtn} from "../../assets/cartRemoveBtn.svg";
 
 const CartItem = () => {
     return (
         <div className="cartItem">
-            <img src={img1} alt="cart Item"/>
+            <img src={""} alt="cart Item"/>
             <div className="cartItemInfo">
                 <h3>Мужские Кроссовки Nike Air Max 270</h3>
                 <b>100 USD</b>
