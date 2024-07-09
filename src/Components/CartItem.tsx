b6e08028fd876ab70099c7a127edd04a01385348
Aleeg0 <Sankafoxe@gmail.com>
2024-07-09 16:53:00 +0300

    add cartItem component

---
+++ src/Components/CartItem.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import img1 from "../assets/images/image1.jpg"
+import {ReactComponent as RemoveBtn} from "../assets/cartRemoveBtn.svg";
+
+const CartItem = () => {
+    return (
+        <div className="cartItem">
+            <img src={img1} alt="cart Item"/>
+            <div className="cartItemInfo">
+                <h3>Мужские Кроссовки Nike Air Max 270</h3>
+                <b>100 USD</b>
+            </div>
+            <button
+                type="button"
+            >
+                <RemoveBtn/>
+            </button>
+        </div>
+    );
+};
+
+export default CartItem;
