d5f3aa0125943855e6d30695ad88a7431a770e5c
Aleeg0 <Sankafoxe@gmail.com>
2024-07-09 23:21:23 +0300

    add comparer

--- src/Models/Sneaker.ts
+++ src/Models/Sneaker.ts
@@ -4,3 +4,7 @@ export default interface Sneaker {
     price: number,
     imgURL: string
 }
+
+export function isEqual(sneaker1: Sneaker, sneaker2: Sneaker): boolean {
+    return sneaker2.name === sneaker1.name && sneaker2.imgURL === sneaker1.imgURL && sneaker2.price === sneaker1.price
+}
