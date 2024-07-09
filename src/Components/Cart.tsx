f9aec7a233a8e5858aad072a811c3994edbe54d6
Aleeg0 <Sankafoxe@gmail.com>
2024-07-09 16:52:41 +0300

    add cart component

---
+++ src/Components/Cart.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import CartItem from "./CartItem";
+import {ReactComponent as OrderArrow} from "../assets/orderArrow.svg";
+
+const Cart = () => {
+    return (
+        <div className="overlay">
+            <div className="cartBar">
+                <h2>Cart</h2>
+                <div className="cartItems">
+                    <CartItem/>
+                    <CartItem/>
+                    <CartItem/>
+                </div>
+                <div>
+                    <ul>
+                        <li>
+                            <span>Total:</span>
+                            <hr/>
+                            <b>500 USD</b>
+                        </li>
+                        <li>
+                            <span>Tax 5%:</span>
+                            <hr/>
+                            <b>5 USD</b>
+                        </li>
+                    </ul>
+                    <button
+                        type="button"
+                        className="orderBtn"
+                    >Place an order <OrderArrow/>
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Cart;
