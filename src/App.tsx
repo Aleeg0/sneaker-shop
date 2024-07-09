dd7567906e232a5f10d3c50746d4a235845a7641
Aleeg0 <Sankafoxe@gmail.com>
2024-07-09 16:54:10 +0300

    add input for app

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,42 @@
 import React from 'react';
 import Header from "./Components/Header";
-import "./index.css"
 import SneakerCard from "./Components/SneakerCard";
+import {ReactComponent as FindLogo} from "./assets/finder.svg";
+import Sneaker from "./Models/Sneaker";
+import "./index.css"
 
 function App() {
+    const [sneakersList,setSnieakersList] = React.useState<Sneaker[]>([]);
+
     return (
-      <div className="wrapper">
-          <Header/>
-          <main>
-              <div className="d-flex justify-between">
-                  <h1>ALL Sneakers</h1>
-                  <input />
-              </div>
-              <div className="content">
-                  <SneakerCard/>
-                  <SneakerCard/>
-                  <SneakerCard/>
-                  <SneakerCard/>
-                  <SneakerCard/>
-                  <SneakerCard/>
-                  <SneakerCard/>
-                  <SneakerCard/>
-              </div>
-          </main>
-      </div>
-)
-    ;
+        <div className="wrapper">
+
+            <Header/>
+            <main>
+                <div className="contentInfo">
+                    <h1>ALL Sneakers</h1>
+                    <div className="searcher">
+                        <FindLogo/>
+                        <input
+                            type="text"
+                            placeholder="Search..."
+                        />
+                    </div>
+                </div>
+                <div className="content">
+                    <SneakerCard/>
+                    <SneakerCard/>
+                    <SneakerCard/>
+                    <SneakerCard/>
+                    <SneakerCard/>
+                    <SneakerCard/>
+                    <SneakerCard/>
+                    <SneakerCard/>
+                </div>
+            </main>
+        </div>
+    )
+        ;
 }
 
 export default App;
