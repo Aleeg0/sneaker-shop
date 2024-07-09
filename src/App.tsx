799665d3ad3abee15bbccfd20d0c2976ddbf1d69
Aleeg0 <Sankafoxe@gmail.com>
2024-07-09 19:20:22 +0300

    add get logic from backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Header from "./Components/Header";
 import SneakerCard from "./Components/SneakerCard";
 import {ReactComponent as FindLogo} from "./assets/finder.svg";
 import Sneaker from "./Models/Sneaker";
 import "./index.css"
 import Cart from "./Components/Cart";
+import axios from "axios";
 
 function App() {
-    const [sneakersList,setSnieakersList] = React.useState<Sneaker[]>([]);
+    const [sneakersList,setSneakersList] = React.useState<Sneaker[]>([]);
+    const [isCartOpen, setIsCartOpen] = React.useState<boolean>(false);
+
+    useEffect(() => {
+        axios.get("https://0f8af2c588831550.mokky.dev/sneakers")
+            .then(res => {
+                console.log(res.data);
+                setSneakersList(res.data);
+            })
+            .catch(err => console.log(err))
+    }, []);
 
     return (
         <div className="wrapper">
-            <Cart/>
-            <Header/>
+            <Cart
+                isOpen={isCartOpen}
+                onCloseCart={setIsCartOpen}
+            />
+            <Header onClickCart={setIsCartOpen}/>
             <main>
                 <div className="contentInfo">
                     <h1>ALL Sneakers</h1>
@@ -25,14 +39,14 @@ function App() {
                     </div>
                 </div>
                 <div className="content">
-                    <SneakerCard/>
-                    <SneakerCard/>
-                    <SneakerCard/>
-                    <SneakerCard/>
-                    <SneakerCard/>
-                    <SneakerCard/>
-                    <SneakerCard/>
-                    <SneakerCard/>
+                    {sneakersList.map((sneaker) =>
+                        <SneakerCard
+                            key={sneaker.id}
+                            name={sneaker.name}
+                            price={sneaker.price}
+                            imgURL={sneaker.imgURL}
+                        />
+                    )}
                 </div>
             </main>
         </div>
