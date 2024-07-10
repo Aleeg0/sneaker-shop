c21b28fb37f332c39c9e36de2211df8eea39bf33
Aleeg0 <Sankafoxe@gmail.com>
2024-07-10 21:49:11 +0300

    fix searcher

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {FC, useEffect} from 'react';
 import Header from "../Components/Header";
 import SneakerCard from "../Components/SneakerCard";
 import {ReactComponent as FindLogo} from "../assets/finder.svg";
@@ -6,36 +6,38 @@ import axios from "axios";
 import {useSneakers} from "../Hooks/Sneakers/useSneakers";
 import {useCartSneakers} from "../Hooks/Cart/useCartSneakers";
 import {useFavoriteSneakers} from "../Hooks/Favorite/useFavoriteSneakers";
-import Cart from "../Components/Cart";
+import {Outlet} from "react-router-dom";
+
 
 const Home = () => {
-    const {sneakers} = useSneakers();
+    const {sneakers, setSneakers} = useSneakers();
     const {cartSneakers,onCartAction} = useCartSneakers();
     const {favoriteSneakers,onFavoriteAction} = useFavoriteSneakers();
-
-    const [isCartOpen, setIsCartOpen] = React.useState<boolean>(false);
     const [searchValue,setSearchValue] = React.useState<string>("")
 
-    /*
+
     useEffect(() => {
-        let searchParam = "";
-        if (searchValue) {
-            searchParam = `?name=*${searchValue}*`
+        async function Search() {
+            let searchParam = "";
+            if (searchValue) {
+                searchParam = `?name=*${searchValue}*`
+            }
+            try {
+                const sneakers = await axios.get(`https://0f8af2c588831550.mokky.dev/sneakers` + searchParam)
+                setSneakers(sneakers.data);
+            }
+            catch (err) {
+                alert(err);
+                console.log("backend filtration err:  ", err);
+            }
         }
-        axios.get(`https://0f8af2c588831550.mokky.dev/sneakers` + searchParam)
-            .then(res => setSneakers(c => res.data));
+        Search();
     }, [searchValue]);
-     */
 
     return (
         <>
-            <Cart
-                isOpen={isCartOpen}
-                onCloseCart={setIsCartOpen}
-                sneakersList={cartSneakers}
-                onCartRemove={onCartAction}
-            />
-            <Header onClickCart={setIsCartOpen}/>
+            <Outlet/>
+            <Header/>
             <main>
                 <div className="contentInfo">
                     <h1>ALL Sneakers</h1>
