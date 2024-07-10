0727deed921c5d237811c05564f5516c45333da1
Aleeg0 <Sankafoxe@gmail.com>
2024-07-10 22:14:25 +0300

    make more readable

--- src/Hooks/Favorite/useFavoriteSneakers.tsx
+++ src/Hooks/Favorite/useFavoriteSneakers.tsx
@@ -5,7 +5,9 @@ import {AppContext, IAppContext} from "../AppContext";
 export const useFavoriteSneakers = () => {
     const state = useContext<IAppContext|null>(AppContext);
     if (state) {
-        return {favoriteSneakers: state.favoriteSneakers, onFavoriteAction: state.onFavoriteAction};
+        return {favoriteSneakers: state.favoriteSneakers,
+          onFavoriteAction: state.onFavoriteAction
+        };
     }
     throw new Error("useFavoriteSneakers is undefined");
 
