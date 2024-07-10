1c58b2ecae8461cf02fd79dcfdb5450451861392
Aleeg0 <Sankafoxe@gmail.com>
2024-07-10 20:00:43 +0300

    add favoriteSneakers useState

---
+++ src/Hooks/Favorite/useFavoriteSneakers.tsx
@@ -0,0 +1,12 @@
+import {useContext} from "react";
+import {AppContext, IAppContext} from "../AppContext";
+
+
+export const useFavoriteSneakers = () => {
+    const state = useContext<IAppContext|null>(AppContext);
+    if (state) {
+        return {favoriteSneakers: state.favoriteSneakers, onFavoriteAction: state.onFavoriteAction};
+    }
+    throw new Error("useFavoriteSneakers is undefined");
+
+}
