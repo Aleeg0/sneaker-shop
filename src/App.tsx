8186453115a8bf079d35d773091e0a0b733e15d4
Aleeg0 <Sankafoxe@gmail.com>
2024-07-10 20:06:07 +0300

    make hard refactoring with requests to backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,107 +1,113 @@
 import React, {useEffect} from 'react';
-import Header from "./Components/Header";
-import SneakerCard from "./Components/SneakerCard";
-import {ReactComponent as FindLogo} from "./assets/finder.svg";
-import Sneaker, {isEqual} from "./Models/Sneaker";
 import "./index.css"
-import Cart from "./Components/Cart";
 import axios from "axios";
+import {Route, Routes} from "react-router-dom";
+import Home from "./Pages/Home";
+import Favorite from "./Pages/Favorite";
+import Profile from "./Pages/Profile";
+import Sneaker from "./Models/Sneaker";
+import {AppContext} from "./Hooks/AppContext";
 
 function App() {
     // all sneakers stats
-    const [sneakersList,setSneakersList] = React.useState<Sneaker[]>([]);
+    const [sneakers,setSneakers] = React.useState<Sneaker[]>([]);
     const [cartSneakers,setCartSneakers] = React.useState<Sneaker[]>([]);
     const [favoriteSneakers,setFavoriteSneakers] = React.useState<Sneaker[]>([]);
-    // for
-    const [isCartOpen, setIsCartOpen] = React.useState<boolean>(false);
-    const [searchValue,setSearchValue] = React.useState<string>("")
 
     useEffect(() => {
-        axios.get("https://0f8af2c588831550.mokky.dev/sneakers")
-            .then(res => {
-                console.log(res.data);
-                setSneakersList(res.data);
-            })
-            .catch(err => console.log(err))
-        axios.get("https://0f8af2c588831550.mokky.dev/cart")
-            .then(res => {
-                console.log(res.data);
-                setCartSneakers(res.data);
-            })
+        async function fetchingData () {
+            try {
+                const sneakers = await axios.get("https://0f8af2c588831550.mokky.dev/sneakers");
+                const cartSneakers = await axios.get("https://0f8af2c588831550.mokky.dev/cart");
+                const favoriteSneakers = await axios.get("https://0f8af2c588831550.mokky.dev/favorites");
+
+                console.log(sneakers.data);
+                console.log(cartSneakers.data);
+                console.log(favoriteSneakers.data);
+
+                setSneakers(sneakers.data);
+                setCartSneakers(cartSneakers.data);
+                setFavoriteSneakers(favoriteSneakers.data);
+
+            } catch (err){
+                alert("Error getting data...");
+                console.error(err);
+            }
+        }
+        fetchingData();
     }, []);
 
-    const onCartAction = async (sneaker: Sneaker, isAdded: boolean) => {
-        // if we need to remove
-        if (isAdded){
-            await onRemoveFromCart(cartSneakers.find(curSneaker => isEqual(curSneaker,sneaker))!);
-        }
-        // if we need to add
-        else{
-            try {
+    const onCartAction = async (sneaker: Sneaker) => {
+        try {
+            // if we need to remove
+            if (cartSneakers.some(curSneaker => curSneaker.sneakerId === sneaker.sneakerId)) {
+                setCartSneakers(c => c.filter(curSneaker => curSneaker.sneakerId !== sneaker.sneakerId));
+                const s = cartSneakers.find(curSneaker => curSneaker.sneakerId === sneaker.sneakerId);
+                console.log(s!.id);
+                await axios.delete("https://0f8af2c588831550.mokky.dev/cart/"
+                    + cartSneakers.find(curSneaker => curSneaker.sneakerId === sneaker.sneakerId)!.id
+                );
+            }
+            // if we need to add
+            else {
                 const {data} = await axios.post("https://0f8af2c588831550.mokky.dev/cart", sneaker);
+                console.log(data.id, data.sneakerId);
                 setCartSneakers(c => [...c,data]);
             }
-            catch (err) {
-                console.log(`failed:, ${err}`);
+        }
+        catch (err) {
+            alert("onCartAction request failed");
+            console.error(`failed:, ${err}`);
+        }
+    }
+
+    const onFavoriteAction = async (sneaker: Sneaker) => {
+        try {
+            // if we need to remove
+            if (cartSneakers.some(curSneaker => curSneaker.sneakerId === sneaker.sneakerId)) {
+                setFavoriteSneakers(c => c.filter(curSneaker => curSneaker.sneakerId !== sneaker.sneakerId));
+                await axios.delete(`https://0f8af2c588831550.mokky.dev/cart/
+                    ${favoriteSneakers.find(curSneaker => curSneaker.sneakerId !== sneaker.sneakerId)!.id}`
+                );
+            }
+            // if we need to add
+            else {
+                const {data} = await axios.post("https://0f8af2c588831550.mokky.dev/cart", sneaker);
+                setFavoriteSneakers(c => [...c, data]);
             }
         }
-    }
-
-    const onRemoveFromCart = async (sneaker: Sneaker) => {
-        try {
-            await axios.delete(`https://0f8af2c588831550.mokky.dev/cart/${sneaker.id}`);
-            setCartSneakers(c => c.filter(curSneaker => curSneaker.id !== sneaker.id));
-        }
-        catch(err) {
-            console.log(`failed:, ${err}`);
+        catch (err) {
+            alert("onFavoriteAction request failed");
+            console.error(`failed:, ${err}`);
         }
     }
-
-    useEffect(() => {
-        let searchParam = "";
-        if (searchValue) {
-            searchParam = `?name=*${searchValue}*`
-        }
-        axios.get(`https://0f8af2c588831550.mokky.dev/sneakers` + searchParam)
-            .then(res => setSneakersList(c => res.data));
-    }, [searchValue]);
 
     return (
-        <div className="wrapper">
-            <Cart
-                isOpen={isCartOpen}
-                onCloseCart={setIsCartOpen}
-                sneakersList={cartSneakers}
-                onCartRemove={onRemoveFromCart}
-            />
-            <Header onClickCart={setIsCartOpen}/>
-            <main>
-                <div className="contentInfo">
-                    <h1>ALL Sneakers</h1>
-                    <div className="searcher">
-                        <FindLogo/>
-                        <input
-                            type="text"
-                            placeholder="Search..."
-                            value={searchValue}
-                            onChange={(event) => setSearchValue(event.target.value)}
-                        />
-                    </div>
-                </div>
-                <div className="content">
-
-                    {sneakersList.map((sneaker) =>
-                        <SneakerCard
-                            {...sneaker}
-                            isAdd={!!cartSneakers.find(s => isEqual(s,sneaker))}
-                            onAction={onCartAction}
-                        />
-                    )}
-                </div>
-            </main>
-        </div>
-    )
-        ;
+        <AppContext.Provider value={{sneakers, cartSneakers, favoriteSneakers, onCartAction, onFavoriteAction}}>
+            <div className="wrapper">
+                <Routes>
+                    <Route
+                        path="/"
+                        element={
+                            <Home/>
+                        }
+                    />
+                    <Route
+                        path="/favorites"
+                        element={
+                            <Favorite/>
+                        }
+                    />
+                    <Route
+                        path="/profile"
+                        element={
+                            <Profile/>
+                        }
+                    />
+                </Routes>
+            </div>
+        </AppContext.Provider>
+    );
 }
 
 export default App;
