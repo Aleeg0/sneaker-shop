b0ef8672f68f268b40b886fd66b2338b60e59ffc
Aleeg0 <Sankafoxe@gmail.com>
2024-07-10 23:16:25 +0300

    make code more clear

--- src/Components/SneakerCard/index.tsx
+++ src/Components/SneakerCard/index.tsx
@@ -5,59 +5,59 @@ import {ReactComponent as CartRemoveBtn} from "../../assets/mainRemoveBtn.svg"
 import Sneaker from "../../Models/Sneaker";
 
 interface SneakerCardProps {
-    sneaker: Sneaker,
-    onCartAction?: (sneaker: Sneaker) => void,
-    onFavoriteAction?: (sneaker: Sneaker) => void,
-    isAdded: boolean,
-    isFavorite: boolean
+  sneaker: Sneaker,
+  onCartAction?: (sneaker: Sneaker) => void,
+  onFavoriteAction?: (sneaker: Sneaker) => void,
+  isAdded: boolean,
+  isFavorite: boolean
 }
 
 const SneakerCard: FC<SneakerCardProps> = ({
-    sneaker,
-    onCartAction = null,
-    onFavoriteAction = null,
-    isAdded,
-    isFavorite
-}) => {
+                                             sneaker,
+                                             onCartAction = null,
+                                             onFavoriteAction = null,
+                                             isAdded,
+                                             isFavorite
+                                           }) => {
 
-    const onClickCartAction = () => {
-        onCartAction!(sneaker);
-    }
+  const onClickCartAction = () => {
+    onCartAction!(sneaker);
+  }
 
-    const onClickFavoriteAction = () => {
-        onFavoriteAction!(sneaker)
-    }
+  const onClickFavoriteAction = () => {
+    onFavoriteAction!(sneaker)
+  }
 
-    return (
-        <div className="card">
-            {onFavoriteAction &&
-                <button
-                    type="button"
-                    className={`favorite ${isFavorite && "active"}`}
-                    onClick={onClickFavoriteAction}
-                >
-                    <FavoriteBtn/>
-                </button>
-            }
-            <img src={sneaker.imgURL} alt="sneaker"/>
-            <p>{sneaker.name}</p>
-            <div className="d-flex justify-between fa-center">
-                <div className="d-flex fd-column">
-                <span>Price:</span>
-                    <b>{sneaker.price} USD</b>
-                </div>
-                {onCartAction &&
-                    <button
-                        type="button"
-                        className={`cartAction ${isAdded ? "removeBtn" : "addBtn"}`}
-                        onClick={onClickCartAction}
-                    >
-                        {isAdded ? <CartRemoveBtn/> : <CartAddBtn/>}
-                    </button>
-                }
-            </div>
+  return (
+    <div className="card">
+      {onFavoriteAction &&
+          <button
+              type="button"
+              className={`favorite ${isFavorite && "active"}`}
+              onClick={onClickFavoriteAction}
+          >
+              <FavoriteBtn/>
+          </button>
+      }
+      <img src={sneaker.imgURL} alt="sneaker"/>
+      <p>{sneaker.name}</p>
+      <div className="d-flex justify-between fa-center">
+        <div className="d-flex fd-column">
+          <span>Price:</span>
+          <b>{sneaker.price} USD</b>
         </div>
-    );
+        {onCartAction &&
+            <button
+                type="button"
+                className={`cartAction ${isAdded ? "removeBtn" : "addBtn"}`}
+                onClick={onClickCartAction}
+            >
+              {isAdded ? <CartRemoveBtn/> : <CartAddBtn/>}
+            </button>
+        }
+      </div>
+    </div>
+  );
 };
 
 export default SneakerCard;
