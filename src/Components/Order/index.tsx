caaf054b72c2d8e8505836c6b44df9d255c0c670
Aleeg0 <Sankafoxe@gmail.com>
2024-07-12 21:08:41 +0300

    add order component

---
+++ src/Components/Order/index.tsx
@@ -0,0 +1,35 @@
+import React, {FC} from 'react';
+import {Sneaker} from "../../Models/Sneaker";
+import SneakerCard from "../SneakerCard";
+import styles from "./order.module.scss"
+
+interface OrderProps {
+  id: number,
+  items: Sneaker[]
+}
+
+const Order: FC<OrderProps> = ({id,items}) => {
+  const s: Sneaker = {
+    id: 1,
+    sneakerId: 1,
+    price: 100,
+    name: 'Мужские Кроссовки Under Armour Curry 8',
+    imgURL: "./sneakers/sneaker-1.png"
+  }
+  
+  return (
+    <div className={styles.order}>
+      <h2>Order №{id}</h2>
+      <div className={`${styles.sneakers} ${styles.empty}`}>
+        {items.map((sneaker, index) =>
+          <SneakerCard
+            key={index}
+            sneaker={sneaker}
+          />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Order;
