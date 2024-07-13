6bec91f6f65b9d67f9c3184c5a38dbeeb4650282
Aleeg0 <Sankafoxe@gmail.com>
2024-07-13 15:02:14 +0300

    add loading logic

--- src/Pages/Profile/index.tsx
+++ src/Pages/Profile/index.tsx
@@ -1,6 +1,6 @@
 import React, {useEffect} from 'react';
 import Header from "../../Components/Header";
-import {Link, useNavigate} from "react-router-dom";
+import {Link} from "react-router-dom";
 import styles from "../_pages.module.scss";
 import { ReactComponent as GoBack} from "../../assets/goBack.svg";
 import InfoCard from "../../Components/InfoCard";
@@ -12,8 +12,10 @@ import Cart from "../../Components/Cart";
 
 const Profile = () => {
   const [orders, setOrders] = React.useState<IOrder[]>([]);
+  const [isLoading, setIsLoading] = React.useState(true);
 
   useEffect(() => {
+    setIsLoading(true);
     const fetchingData = async () => {
       try {
         const {data} = await axios.get("https://0f8af2c588831550.mokky.dev/orders");
@@ -24,7 +26,7 @@ const Profile = () => {
         console.log(err);
       }
     }
-    fetchingData();
+    fetchingData().finally(() => setIsLoading(false));
   }, []);
 
   console.log("render");
@@ -39,19 +41,29 @@ const Profile = () => {
           </Link>
           <h1>Orders</h1>
         </div>
-        <div className={`${styles.profile} ${styles.content}`}>
-          {orders.length > 0 ?
-            orders.map((order,index) =>
-              <Order key={index} {...order}/>
-            )
+        <div className={
+            [
+              styles.profile,
+              styles.content,
+              orders.length > 0 ? "" : styles.empty
+            ].join(" ")
+          }>
+          {isLoading ?
+            <>
+            </>
             :
-            <InfoCard
-              title={"No favorites :("}
-              description={"You didn't add anything to your favorites"}
-              imgName={"noFavorite.svg"}
-              altText={"sedImg"}
-              to={""}
-            />
+            orders.length > 0 ?
+              orders.map((order,index) =>
+                <Order key={index} {...order}/>
+              )
+              :
+              <InfoCard
+                title={"You don't have any orders"}
+                description={"Are you a beggar? \nPlace at least one order."}
+                imgName={"noOrder.svg"}
+                altText={"cryImg"}
+                to={"/"}
+              />
           }
         </div>
       </main>
