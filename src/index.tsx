882e7f99050e782163d404f1ca47002dcd366d98
Aleeg0 <Sankafoxe@gmail.com>
2024-07-13 16:55:57 +0300

    fix router basename

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter as Route} from "react-router-dom";
+import {BrowserRouter as Router} from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
 root.render(
-    <Route>
+    <Router basename="sneaker-shop">
         <App/>
-    </Route>
+    </Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
