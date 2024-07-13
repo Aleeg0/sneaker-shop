5158a9b44b43ab8325c1435e56c6f0012a4c570e
Aleeg0 <Sankafoxe@gmail.com>
2024-07-13 15:12:49 +0300

    add orders to appContext

--- src/Hooks/AppContext.tsx
+++ src/Hooks/AppContext.tsx
@@ -1,17 +1,23 @@
 import React, {createContext} from "react";
 import {ISneaker} from "../Models/ISneaker";
+import {IOrder} from "../Models/IOrder";
 
 export interface IAppContext{
+    // all sneakers
     sneakers: ISneaker[],
     setSneakers: (sneaker: React.SetStateAction<ISneaker[]>) => void,
+    // cart sneakers
     cartSneakers: ISneaker[],
     setCartSneakers: (sneaker: React.SetStateAction<ISneaker[]>) => void,
+    onCartAction: (sneaker: ISneaker) => void,
     isCartOpened: boolean,
     setIsCartOpened: (isOpened: boolean) => void,
+    // favorite sneakers
     favoriteSneakers: ISneaker[],
-    //setFavoriteSneakers: (sneaker: ISneaker) =>  React.Dispatch<React.SetStateAction<ISneaker[]>>,
-    onCartAction: (sneaker: ISneaker) => void,
     onFavoriteAction: (sneaker: ISneaker) => void,
+    // orders
+    orders: IOrder[],
+    // isLoading data
     isLoading: boolean
 }
 
